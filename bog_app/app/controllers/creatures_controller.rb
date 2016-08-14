ddfffe42c8cbe036f12a18bc75e650a435113fe0
billhimmelsbach <whimmels@gmail.com>
2016-08-13 20:08:45 -0700

    finished first iteration

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -3,4 +3,42 @@ class CreaturesController < ApplicationController
     @creatures = Creature.all
     render :index
   end
+  def new
+    @creature = Creature.new
+    render :new
+  end
+  def create
+    @creature = Creature.new(creature_params)
+    if @creature.save
+      redirect_to creature_path(@creature)
+    end
+  end
+  def show
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+    render :show
+  end
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+    render :edit
+  end
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+  def destroy
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.destroy
+    redirect_to creatures_path
+  end
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
+
 end
